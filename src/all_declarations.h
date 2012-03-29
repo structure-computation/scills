eb05962a0dfc458374216ed0ba46ef8f4af9210c
aissam <lekhder.aissam@gmail.com>
2012-03-29 14:07:45 +0200

    Rajout du fichier all_declations.h (oublie au dernier commit) et suppression du fichier src/UTILITAIRES/reconstruction_dep_contrainte.h qui etait inutile.

---
+++ src/all_declarations.h
@@ -0,0 +1,18 @@
+#ifndef ALL_DECLARATIONS
+#define ALL_DECLARATIONS
+
+// SC_create
+#include "COMPUTE/DataUser.h"
+#include "COMPUTE/FieldStructureUser.h"
+
+#include "DEFINITIONS/DEFINITION_declarations.h"
+#include "ITERATIONS/ITERATIONS_declarations.h"
+#include "MAILLAGE/multiscale_geometry_mesh.h"
+
+#include "../LMT/include/containers/vec.h"
+#include "../LMT/include/containers/vec_mt.h"
+#include "../LMT/include/containers/vecpointedvalues.h"
+using namespace LMT;
+using namespace Metil;
+
+#endif
